7efd6f6b25d2b2de291af9f8dfa6b2950b6bd3d4
ali al-baiti <albaityali3@yahoo.com>
2019-02-11 21:12:25 +0300

    push to heroku

--- fb/settings/production.py
+++ fb/settings/production.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['baitran.herokuapp.com']
+ALLOWED_HOSTS = ['alibook.herokuapp.com']
 
 AUTHENTICATION_BACKENDS = [
     'axes.backends.AxesModelBackend',
